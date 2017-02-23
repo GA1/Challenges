ae3f1407554ee1f8d9cfd3a503f0d23fdfbe79ea
GA1 <nvkga1@gmail.com>
2017-02-23 15:52:27 +0100

    Assume characters [a-z] are used

--- src/main/java/com/challenges/AnagramSolver.java
+++ src/main/java/com/challenges/AnagramSolver.java
@@ -2,6 +2,7 @@ package com.challenges;
 
 public class AnagramSolver {
 
+    // Assume the possible characters are a,b,c...,x,y,z
     public boolean areAnagrams(String word1, String word2) {
         return false;
     }
